b6e19d3923e2be9279b99e8f4a4d05b71f624e6f
Ketan Verma <ketanverma1511@gmail.com>
2026-09-15 20:26:44 +0530

    Add: find-smallest-common-element-in-all-rows - submission-0

---
+++ Data Structures & Algorithms/find-smallest-common-element-in-all-rows/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestCommonElement(vector<vector<int>>& mat) {
+        map<int,int> mp;
+        int ans=-1;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                mp[mat[i][j]]++;
+            }
+        }
+        for(auto it:mp){
+            if(it.second==mat.size()){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
